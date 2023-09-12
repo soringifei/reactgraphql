bf9acc9ac3b2119b856f627d3ec5d63ab92160f6
GBerGG <sgifei@yahoo.com>
2023-09-12 20:02:13 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import github from './db';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+	let [userName, setUserName] = useState('');
+	useEffect(() => {
+		const githubQuery = {
+			query: `
+			{
+				viewer {
+				  login
+				}
+			}
+		`,
+		};
+
+		fetch(github.baseUrl, {
+			method: 'POST',
+			header: github.headers,
+			body: JSON.stringify(githubQuery),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				setUserName(data.data.viewer.name);
+				console.log(data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	});
 	return (
 		<div className="App container mt-5">
 			<h1 className="text-primary">
 				<i className="bi bi-diagram-2-fill"></i> Repos
 			</h1>
+			<p>Hey there {userName}</p>
 		</div>
 	);
 }
